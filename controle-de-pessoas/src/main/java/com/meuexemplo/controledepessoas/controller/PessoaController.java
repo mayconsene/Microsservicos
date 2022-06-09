f892821732e2b5067adeeff5c3038c773be55d67
github.com/mayconsene <mayconmoraessene@gmail.com>
2022-06-09 18:41:49 -0300

    09-06-2022 - Eureka + Zuul + Pessoa OK

---
+++ controle-de-pessoas/src/main/java/com/meuexemplo/controledepessoas/controller/PessoaController.java
@@ -0,0 +1,33 @@
+package com.meuexemplo.controledepessoas.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.meuexemplo.controledepessoas.model.Pessoa;
+
+@RestController
+@RequestMapping("/api/pessoas")
+public class PessoaController {
+
+    @GetMapping
+    public List<Pessoa> obterTodos(){
+        Pessoa p1 = new Pessoa();
+        p1.setNome("João");
+        p1.setEmail("joao@gmail.com");
+        p1.setTelefone("16999978865");
+
+        Pessoa p2 = new Pessoa();
+
+        ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+
+        pessoas.add(p1);
+        pessoas.add(p2);
+
+        return pessoas;
+    }
+    
+}
